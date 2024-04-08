b5c359b71db5f23eb469cd3a4b63fc58858c77fd
ictsu631310450 <109814371+ictsu631310450@users.noreply.github.com>
2024-04-08 08:52:24 +0700

    Reset

--- projects.php
+++
@@ -1,8 +0,0 @@
-<?php
-$projects = [
-    ['name' => 'Project 1', 'url' => 'project1.php'],
-    ['name' => 'Project 2', 'url' => 'project2.php'],
-    ['name' => 'Project 3', 'url' => 'project3.php']
-    // เพิ่มโปรเจ็กต์เพิ่มเติมตามต้องการ
-];
-?>
